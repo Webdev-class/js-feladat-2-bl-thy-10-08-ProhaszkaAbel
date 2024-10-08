0af5514de0ba16c713a8a1df2071db6af738c6e6
Prohaszka Abel <prohaszka.abel05@gmail.com>
2024-10-08 08:19:17 +0200

    F1, F2 done

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,29 @@
+const historyQueryString = "https://archive-api.open-meteo.com/v1/archive?";
 
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+async function loadFetch(longitude, latitude, date) {
+    let data = null;
+    
+    await fetch(makeURL(longitude, latitude, date))
+    .then(x => x.json())
+    .then(x => data = x)
+    .catch(x => console.log("Error happened"));
+
+    return data;
+}
+
+async function averageTemperature(longitude, latitude, date) {
+    let data = await loadFetch(longitude, latitude, date);
+    let array = data.hourly.temperature_2m;
+    let avg = array.reduce((a, b) => a + b) / array.length;
+    return Math.floor(avg);
+}
+
+function makeURL(longitude, latitude, date) {
+    return `${historyQueryString}longitude=${longitude}&latitude=${latitude}&start_date=${date}&end_date=${date}&hourly=temperature_2m`;
+}
+
 async function f1 (lat, long, timeStr) {
-    //TODO
+    return averageTemperature(long, lat, timeStr);
 }
 
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -3,14 +3,31 @@ Készítsd el az előző feladat azon változatát ahol megadjuk az óraszámot 
 Csak az adott órára vonatkozó hőmérséklettel térjen vissza a függvény.
 */
 
+const historyQueryString = "https://archive-api.open-meteo.com/v1/archive?";
+
+async function loadFetch(longitude, latitude, date) {
+    let data = null;
+    
+    await fetch(makeURL(longitude, latitude, date))
+    .then(x => x.json())
+    .then(x => data = x)
+    .catch(x => console.log("Error happened"));
+
+    return data;
+}
+
+async function getTemperatureByHour(longitude, latitude, date, hour) {
+    let data = await loadFetch(longitude, latitude, date);
+    return data.hourly.temperature_2m[hour];
+}
+
+function makeURL(longitude, latitude, date) {
+    return `${historyQueryString}longitude=${longitude}&latitude=${latitude}&start_date=${date}&end_date=${date}&hourly=temperature_2m`;
+}
+
 const locations = require('../locations.json');
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    const str = APIAdress + `latitude=${lat}&longitude=${long}` +
-        `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
-    const rArr = await fetch(str)
-        .then(data => data.json());
-    return rArr.hourly.temperature_2m.at(hour);
+    return getTemperatureByHour(long, lat, timeStr, hour);
 }
 module.exports = f2;
